f218232845c728bd9eb21253874f33b6fe24e602
timvdweijer <37894170+timvdweijer@users.noreply.github.com>
2018-03-29 11:28:34 +0200

    1

--- flow visualization.py
+++ flow visualization.py
@@ -25,18 +25,35 @@ from skimage import measure
 import math
 <<<<<<< HEAD
 stop=time.clock()
+=======
+
+
+# Visualization
+from pyevtk.hl import gridToVTK
+
+
+stop= time.clock()
+>>>>>>> 4839c5139e84c9909f7ef775a834588ce3282498
 print ('\n',int((stop-start)*1000)/1000.,'sec -- imported modules')
 
 #---------------------------------------------------------General setup for program run
 to_load=False          # if true will load already the last calculated Q or lambda dataset
 to_plotly=False        # if true will send the plot to plotly website
 to_matplot=False        # if true will use matplotlib to plot
+<<<<<<< HEAD
 n_elements=60         # number of elements on each side of cube calculated
+=======
+n_elements=92         # number of elements on each side of cube calculated
+>>>>>>> 4839c5139e84c9909f7ef775a834588ce3282498
 to_calc_Q=True          # if true will calc Q on cube with n_elements
 to_calc_Lambda2=False   # if true will calc lambda2 on cube with n_elements
 q_threshold=0.16          # threshold for marching cubes algorithm 
 order_der_method=5       # only 2 or 4 are implemented 3 is 2 but new
+<<<<<<< HEAD
 data_num=0              # 0 for validation dataset, 1 for raw_data_1
+=======
+data_num=0             # 0 for validation dataset, 1 for raw_data_1
+>>>>>>> 4839c5139e84c9909f7ef775a834588ce3282498
 check_data=False        # check only first time you are using dataset
 
 
@@ -50,6 +67,9 @@ data=scipy.io.loadmat(data_set_file, mdict=None, appendmat=True)
 u=data['u']
 v=data['v']
 w=data['w']
+
+ushape = u.shape
+
 
 if check_data:
     ok=True
@@ -66,8 +86,12 @@ if check_data:
     if ok:print('data is rectangular and ok')
     else:print('data not fine')
 
+<<<<<<< HEAD
 #vspace=np.zeros(np.shape(u))
 vspace=np.zeros((n_elements, n_elements, n_elements))
+=======
+vspace=np.zeros((n_elements,n_elements,n_elements))
+>>>>>>> 4839c5139e84c9909f7ef775a834588ce3282498
 delta=2.*math.pi/np.shape(u)[0]
 x_max=np.shape(u)[0]-1
 y_max=np.shape(u)[1]-1
@@ -301,27 +325,28 @@ if not to_load:
     f.write(wri)
     f.write('\n')
     f.close()
-    
-    
 
 
-#   The very end of program...no going behind this line   
-stop= time.clock()
-print ('\n',int((stop-start)*100)/100.,'sec -- finished')
 
-#--------------------------------calculate grid
+
 vspace_shape = np.shape(vspace)
-vtkgridx = np.zeros((vspace_shape[0],vspace_shape[1],vspace_shape[2]))
-vtkgridy = np.zeros((vspace_shape[0],vspace_shape[1],vspace_shape[2]))
-vtkgridz = np.zeros((vspace_shape[0],vspace_shape[1],vspace_shape[2]))
+'''
+xvtk = np.zeros((vspace_shape[0],vspace_shape[1], vspace_shape[2]))
+yvtk = np.zeros((vspace_shape[0],vspace_shape[1], vspace_shape[2]))
+zvtk = np.zeros((vspace_shape[0],vspace_shape[1], vspace_shape[2]))
 for k in range(0,vspace_shape[0]):
     for j in range(0,vspace_shape[1]):
         for i in range(0,vspace_shape[2]):
-            vtkgridx[i,j,k] = i
-            vtkgridy[i,j,k] = j
-            vtkgridz[i,j,k] = k
-#            vspace[i,j,k] = i
-gridToVTK("./calculated data/" + data_set[0] +"-"+ str(n_elements), vtkgridx, vtkgridy, vtkgridz, pointData = {'Q': vspace})
+            xvtk[i,j,k] = i
+            yvtk[i,j,k] = j
+            zvtk[i,j,k] = i
+ '''          
+xvtk = np.arange(0, vspace_shape[0])
+yvtk = np.arange(0, vspace_shape[1])
+zvtk = np.arange(0, vspace_shape[2])
+
+
+gridToVTK("./calculated data/pillfile", xvtk, yvtk, zvtk, pointData = {'pillfile': vspace})
 
 
 ''' sort of useless pieces of code:
