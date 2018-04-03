5c3e5cc21585b1fc3bc66cb86b03fa9bcfeb97a6
mdheer <37894626+mdheer@users.noreply.github.com>
2018-04-03 16:27:21 +0200

    wrong

--- flow_visualization.py
+++ flow_visualization.py
@@ -33,9 +33,9 @@ print ('\n',int((stop-start)*1000)/1000.,'sec -- imported modules')
 to_load=False          # if true will load already the last calculated Q or lambda dataset
 to_plotly=False        # if true will send the plot to plotly website
 to_matplot=False        # if true will use matplotlib to plot
-n_elements=96        # number of elements on each side of cube calculated
-to_calc_Q=True          # if true will calc Q on cube with n_elements
-to_calc_Lambda2=False   # if true will calc lambda2 on cube with n_elements
+n_elements=192        # number of elements on each side of cube calculated
+to_calc_Q=False          # if true will calc Q on cube with n_elements
+to_calc_Lambda2=True   # if true will calc lambda2 on cube with n_elements
 to_calc_vorticity = True  #if true calculate vorticity
 q_threshold=0.16          # threshold for marching cubes algorithm 
 order_der_method=3     # only 2 or 4 are implemented 3 is 2 but new
