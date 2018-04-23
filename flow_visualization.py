7028ccb3346b80484498b04436aea9c845b071aa
mdheer <37894626+mdheer@users.noreply.github.com>
2018-04-23 11:22:51 +0200

    adfkl

--- flow_visualization.py
+++ flow_visualization.py
@@ -34,20 +34,12 @@ idk=False
 to_load=False          # if true will load already the last calculated Q or lambda dataset
 to_plotly=False        # if true will send the plot to plotly website
 to_matplot=False        # if true will use matplotlib to plot
-<<<<<<< HEAD
-n_elements=200        # number of elements on each side of cube calculated
-=======
 n_elements=192        # number of elements on each side of cube calculated
->>>>>>> c493d6bbcb0721648ab88c66b7eb23543fc5ec54
 to_calc_Q=True          # if true will calc Q on cube with n_elements
 to_calc_Lambda2=False   # if true will calc lambda2 on cube with n_elements
 to_calc_vorticity = True  #if true calculate vorticity
 q_threshold=0.16          # threshold for marching cubes algorithm 
-<<<<<<< HEAD
-order_der_method=2     # only 2 or 4 are implemented 3 is 2 but new
-=======
 order_der_method=5       # 2,4 are without looping, 3,5,6 are with looping in 2,4,6 orders respectetively
->>>>>>> c493d6bbcb0721648ab88c66b7eb23543fc5ec54
 data_num=1              # 0 for validation dataset, 1 for raw_data_1
 check_data=False        # check only first time you are using dataset
 
