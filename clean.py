68cdca02027d99a0f9f4380e5c4982e4b8b72220
AndrejKluka <andrej@kluka.sk>
2018-05-08 10:27:57 +0200

    merge

--- clean.py
+++ clean.py
@@ -14,37 +14,17 @@ stop=time.clock()
 print ('\n',int((stop-start)*1000)/1000.,'sec -- imported modules')
 
 ''' To do list '''
-#see why lambda 2 is all positive
-#Think about adding some smart loading
-#add script for paraview thingy and automate
+#automate
 
 
 
 #---------------------------------------------------------General setup for program run
-<<<<<<< HEAD
-to_save=True  
-to_calc_Q=False       # if true will calc Q on cube with n_elements
-to_calc_Lambda2=True   # if true will calc lambda2 on cube with n_elements
-data_num=2              # 0 for validation dataset, 1 for raw_data_1, 2 for data_001
-=======
-<<<<<<< HEAD
-to_save=True  
-to_calc_Q=True       # if true will calc Q on cube with n_elements
-to_calc_Lambda2=False   # if true will calc lambda2 on cube with n_elements
-data_num=2            # 0 for validation dataset, 1 for raw_data_1, 2 for data_001
->>>>>>> 26d8697b22ecb8f89c0470d874dbcfd6df7a2604
-#15.3111 sec
-#37.3022 sec
-=======
-
 Visualization = False
 to_save=False  
 to_calc_Q=False       # if true will calc Q on cube with n_elements
 to_calc_Lambda2=True   # if true will calc lambda2 on cube with n_elements
 data_num=0             # 0 for validation dataset, 1 for raw_data_1, 2 for data_001
 
-
->>>>>>> 776b9bd870321d36f99e305f176d21c07b7bc2ee
 data_set=['validation_Q_l2','raw_data_1','data_001']
 
 #   reading raw dataset and putting them into u,v,w arrays
