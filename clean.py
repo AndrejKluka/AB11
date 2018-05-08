88823201afd3dbc42ba9aa7159af131b3f775d50
Sebas4558944 <37894161+Sebas4558944@users.noreply.github.com>
2018-05-08 12:18:36 +0200

    ;lkjlkj

--- clean.py
+++ clean.py
@@ -18,19 +18,10 @@ print ('\n',int((stop-start)*1000)/1000.,'sec -- imported modules')
 
 #---------------------------------------------------------General setup for program run
 Visualization = False
-<<<<<<< HEAD
-
 to_save=True  
 to_calc_Q=False       # if true will calc Q on cube with n_elements
 to_calc_Lambda2=True   # if true will calc lambda2 on cube with n_elements
 data_num=2            # 0 for validation dataset, 1 for raw_data_1, 2 for data_001
-=======
-to_save=True  
-to_calc_Q=True       # if true will calc Q 
-to_calc_Lambda2=False   # if true will calc lambda2 on cube with n_elements
-data_num=1            # 0 for validation dataset, 1 for raw_data_1, 2 for data_001
->>>>>>> ba5682bfc8dba52c40c13bea4236d418a3b7d445
-
 
 data_set=['validation_Q_l2','raw_data_1','data_001']
 
