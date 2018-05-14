075701dfeb36ff2c7428e55497af3564bd7155b6
Sebas4558944 <37894161+Sebas4558944@users.noreply.github.com>
2018-05-14 13:55:54 +0200

    adsf

--- clean.py
+++ clean.py
@@ -19,8 +19,8 @@ print ('\n',int((stop-start)*1000)/1000.,'sec -- imported modules')
 #---------------------------------------------------------General setup for program run
 Visualization = False
 to_save=True  
-to_calc_Q=False       # if true will calc Q on cube with n_elements
-to_calc_Lambda2=True   # if true will calc lambda2 on cube with n_elements
+to_calc_Q=True       # if true will calc Q on cube with n_elements
+to_calc_Lambda2=False   # if true will calc lambda2 on cube with n_elements
 data_num=2            # 0 for validation dataset, 1 for raw_data_1, 2 for data_001
 
 data_set=['validation_Q_l2','raw_data_1','data_001']
