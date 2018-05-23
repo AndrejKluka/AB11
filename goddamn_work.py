9fd5dfc2133430127783819c75eae124af0ef24e
AndrejKluka <andrej@kluka.sk>
2018-05-23 16:18:17 +0200

    idk

--- goddamn_work.py
+++ goddamn_work.py
@@ -8,4 +8,5 @@ from os import path
 import os
 
 os.chdir("C:\\Program Files\\ParaView 5.5.0-RC3-Qt5-Windows-64bit\\bin\\")
-os.system("pvpython.exe C:\\Users\\timvd\\Documents\\GitHub\\AB11\\trying_to_runpv2.py")
+os.system("pvpython.exe C:\\Users\\Public\\trying_to_runpv2.py")
+
